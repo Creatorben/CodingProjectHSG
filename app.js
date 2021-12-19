36314cbccdf1fa387359f21d7c75b55d2f6d3f41
Beni <bkoller@locorum.ch>
2021-12-19 14:38:04 +0100

    Final

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Test s
+// Weather App JavaScript
 let btn = document.getElementById('submit');
 let eingabe = [];
 
@@ -16,60 +16,47 @@ btn.addEventListener('click', ortSpeichern);
 
 console.log(eingabe);
 
-// Hintergrund je nach Tageszeit wechseln
-let Wettercontainer = document.getElementById('wettercontainer');
-const heute = new Date();
-const zeit = heute.getHours();
+// Hintergrund wechseln nach Standort
+let Wettercontainer = document.querySelector('.bg-container');
+console.log(Wettercontainer);
 
 function hintergrundWechseln() {
   const accessKey = 'ncHA1aJyS2eNi7ZowM_Gxw93tTdi-7nhhC4eOCinzuE';
-  let tageszeit;
-  let neuesbild;
-
-  console.log(zeit);
-
-  if (zeit >= 0 && zeit <= 5) {
-    tageszeit = 'Early_Morning';
-  } else if (zeit >= 6 && zeit <= 10) {
-    tageszeit = 'Morning';
-  } else if (zeit >= 10 && zeit <= 14) {
-    tageszeit = 'Noon';
-  } else if (zeit >= 15 && zeit <= 19) {
-    tageszeit = 'Afternoon';
-  } else if (zeit >= 20 && zeit <= 24) {
-    tageszeit = 'night';
-  }
-
-  let url = `https://api.unsplash.com/search/photos?client_id=${accessKey}&query=${tageszeit}`;
+  let ort = OrtHolen();
+  let url = `https://api.unsplash.com/search/photos?client_id=${accessKey}&query=${ort}`;
   console.log(url);
 
   fetch(url)
     .then(function (response) {
+      console.log(response);
       return response.json();
     })
     .then(function (jsonData) {
-      console.log(jsonData.results[0]);
-      neuesbild = jsonData.results[0].urls.regular;
+      neuesbild = jsonData.results[0].urls.full;
       Wettercontainer.style.backgroundImage = `url("${neuesbild}")`;
     });
 }
 
-window.onload = hintergrundWechseln();
 // ____________________________________________________________ //
 // Wetter für Standort abrufen
 
 const OpenWeatherMapAPI = '8fabe8281116c9e8d15a398f06b23036';
 const Suchergebnisse = 1;
 
-function Standortwetter() {
+function OrtHolen() {
   let ort = sessionStorage.getItem('Ort');
   let ortN = JSON.parse(ort);
-  console.log(ortN[ortN.length - 1]);
   let ortNormal = ortN[ortN.length - 1];
+  return ortNormal;
+}
+
+function Standortwetter() {
+  let ortNormal = OrtHolen();
   // Zuerst aktuellen Ort in Dokument schreiben //
   let döttmuendeortane = document.getElementById('place');
   döttmuendeortane.innerHTML = ortNormal;
   OrtZuKoordinaten(ortNormal);
+  hintergrundWechseln();
 }
 
 btn.addEventListener('click', Standortwetter);
@@ -170,9 +157,9 @@ function IDzuIcon(ID) {
     return `wi-snow`;
   } else if (ID >= 700 && ID <= 790) {
     return `wi-dust`;
-  } else if ((ID = 800 && tagnacht === 'day')) {
+  } else if (ID === 800 && tagnacht === 'day') {
     return `wi-day-sunny`;
-  } else if ((ID = 800 && tagnacht === 'night')) {
+  } else if (ID === 800 && tagnacht === 'night') {
     return `wi-night-clear`;
   } else if (ID >= 800 && ID <= 835) {
     return `wi-${tagnacht}-cloudy`;
@@ -182,6 +169,8 @@ function IDzuIcon(ID) {
 // ____________________________________________________________ //
 
 // Aktuelles Datum und Zeit einfügen
+const heute = new Date();
+const zeit = heute.getHours();
 
 function aktuellesDatum() {
   const monthNames = [
